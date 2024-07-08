22ff110b6e80a251fe53ac2772cd3bec5ce86b1b
KEI$HO <133760530+N-Keisho@users.noreply.github.com>
2024-07-09 01:54:27 +0900

    feature: Add Index

--- app/components/ui/Article/ArticlePage.tsx
+++ app/components/ui/Article/ArticlePage.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import CustomReactMarkdown from './CustomReactMarkdown';
+import Index from './Index';
 import { Article } from '@/types/article';
 
 const ArticlePage: React.FC<{ Article: Article, Badge?: boolean, isActivated?: boolean }> = ({ Article, Badge = false, isActivated = false }) => {
@@ -11,23 +12,24 @@ const ArticlePage: React.FC<{ Article: Article, Badge?: boolean, isActivated?: b
     if (Article.title.includes('???')) title = title.replace('???', '');
     if (Article.title.includes('@@')) title = title.replace('@@', '');
 
-    const _time = Article.created_at.slice(0, 10);
+    const _date = Article.created_at.slice(0, 10);
 
     return (
         <>
-            <div className="flex flex-col justify-center items-center py-4 md:p-14">
-                <div className='bg-white w-11/12 max-w-4xl flex flex-col justify-center items-center px-4 pt-4 pb-2 md:pb-4 md:pt-12 rounded-lg'>
-                    <div className='w-full max-w-3xl'>
+            <div className="flex flex-col mt-4 md:mt-10 mb-10 flex flex-col items-center">
+                <div className='w-11/12 max-w-2xl'>
+                    <div className='w-full'>
                         {Badge && <StatusBadge isActivated={isActivated} category="event" />}
                         <Image src={Article.thumbnail} alt="Thumbnail" className="object-fit w-full" width={600} height={600} />
                     </div>
-                    <div className='w-full max-w-3xl mb-0'>
-                        <p className="text-black opacity-60 text-right text-xs md:text-sm mb-0 mt-1">🕒{_time}</p>
+                    <div className='w-full mb-0'>
+                        <p className="text-black text-left text-xs md:text-sm mb-0 mt-1">{_date}</p>
                     </div>
-                    <div className='bg-secondary-400 mt-5 mb-3 py-1 md:py-2 px-3 md:px-4 w-full max-w-3xl'>
-                        <a className='text-black text-2xl md:text-3xl font-bold'>{title}</a>
+                    <h1>{title}</h1>
+                    <div>
+                        <Index content={Article.body}/>
                     </div>
-                    <div className='max-w-3xl'>
+                    <div>
                         <CustomReactMarkdown content={Article.body} />
                     </div>
                 </div>
