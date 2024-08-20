c4173c4c88660afa929396ef1fb6593944190943
KEI$HO <133760530+N-Keisho@users.noreply.github.com>
2024-08-21 00:42:46 +0900

    design: Add ArrowOutwardIcon & when url is external, open with new tab

--- app/components/ui/button/SimpleButton.tsx
+++ app/components/ui/button/SimpleButton.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import Link from "next/link";
+import ArrowOutwardIcon from "@mui/icons-material/ArrowOutward";
 
 // 単純なボタン
 const SimpleButton: React.FC<{ url: string; text: string }> = ({
   url,
   text,
 }) => {
+  const isExternal = url.startsWith("http");
   return (
     <div className="flex justify-center items-center my-2">
       <Link href={`${url}`} legacyBehavior>
-        <a className="bg-secondary-400 hover:bg-secondary-300 font-bold text-center rounded-md block w-1/3 py-2 text-black border-none">
+        <a
+          className="bg-secondary-400 hover:bg-secondary-300 font-bold text-center rounded-md block w-1/3 py-2 items-center text-black border-none"
+          target={isExternal ? "_blanck" : ""}
+          rel={isExternal ? "noopener noreferrer" : ""}
+        >
           {text}
+          {isExternal && <ArrowOutwardIcon className="text-black pb-0.5" />}
         </a>
       </Link>
     </div>
